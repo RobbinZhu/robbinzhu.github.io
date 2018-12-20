932bfe6c035ea2e4c7263004e52a94dd52e64763
RobbinZhu <robbin.zhu@uuabc.com>
2018-12-20 11:09:04 +0800

    remove game demo

--- game/biz/component/input_state.js
+++
@@ -1,6 +0,0 @@
-export default {
-    Left: 1,
-    Right: 1 << 1,
-    Jump: 1 << 2,
-    Fire: 1 << 3
-}
--- game/biz/hero.js
+++
@@ -1,8 +0,0 @@
-import Node from '../node/node.js'
-import HeroStateComponent from '../component/hero_state_component.js'
-
-class Hero extends Node {
-    constructor() {
-        super()
-    }
-}
